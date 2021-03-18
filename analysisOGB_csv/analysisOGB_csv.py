11affa447bdba31637c87c5720eaf14e71fe2a72
cristina <cristina.isabel.tubert@est.fib.upc.edu>
2021-03-18 21:13:53 +0100

    h

--- analysisOGB_csv/analysisOGB_csv.py
+++ analysisOGB_csv/analysisOGB_csv.py
@@ -49,6 +49,7 @@ def second_split_and_shuffle(nodes_ini, edges_ini):
   return (nodes, edges)
 
 def get_nx_graph(nodes, edges):
+  print(edges)
   undirected = utils.is_undirected(edges)
 
   edge_list = []
