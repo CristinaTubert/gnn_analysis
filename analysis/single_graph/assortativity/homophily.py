f534adcdf77202ada9e71dc70e815299d02006bc
CristinaTubert <cristinaatubert@gmail.com>
2021-05-17 11:52:34 +0000

    results citeseer and products

--- analysis/single_graph/assortativity/homophily.py
+++ analysis/single_graph/assortativity/homophily.py
@@ -38,7 +38,7 @@ def choose_dataset():
 
   ini_dict(fsplit, name)
   
-  return (name, data, dataset, fsplit)
+  return (data, dataset, fsplit)
 
 def choose_globals(num_nodes):
   global SIZE_SPLIT 
@@ -61,24 +61,20 @@ def first_split(data, dataset, fsplit):
 
     edges_ini = dataset[0][0]['edge_index']
 
-    print(dataset[0][1])
-    print(dataset[0][0])
-    # maxi = dataset[0][0]['num_nodes']
-    # print(0 in edges_ini[0])
-    # print(0 in edges_ini[1])
-    # print(maxi in edges_ini[0])
-    # print(maxi in edges_ini[1])
+    features = dataset[0][1]
 
   elif data == 'Plan':
     nodes_ini = list(range(0, len(dataset[0].y))) #always no split
     edges_ini = -1 #no matter
 
-    print(dataset[0].x)
+    features = dataset[0].y
+    
+  name_features = 'y'
+  values_dict['Feature studied'] = name_features
 
-  features = []
-  return (nodes_ini, edges_ini, features)
+  return (nodes_ini, edges_ini, features, name_features)
 
-def second_split(G_all, nodes_ini, i):
+def second_split_OGB(nodes_ini, edges_ini, i):
   if TYPE_SPLIT == 'random':
     rand.shuffle(nodes_ini)
     nodes = nodes_ini[0:SIZE_SPLIT]
@@ -87,32 +83,57 @@ def second_split(G_all, nodes_ini, i):
     j = min( (i+SIZE_SPLIT), len(nodes_ini) )
     nodes = nodes_ini[i:j]
 
-  G = G_all.subgraph(nodes)
-  return G, nodes
+  nodes_tensor = torch.LongTensor([x for x in nodes])
+  edges_tensor = torch.LongTensor([x for x in edges_ini])
 
-def OGB_to_nx(nodes, edges, features):
-  undirected = utils.is_undirected(edges_tensor)
+  edges, _ = utils.subgraph(nodes_tensor, edges_tensor, num_nodes=len(nodes_ini))
+
+  return nodes, edges
+
+def second_split_planetoid(preG, nodes_ini, i):
+  if TYPE_SPLIT == 'random':
+    rand.shuffle(nodes_ini)
+    nodes = nodes_ini[0:SIZE_SPLIT]
+
+  elif TYPE_SPLIT == 'ordered':
+    j = min( (i+SIZE_SPLIT), len(nodes_ini) )
+    nodes = nodes_ini[i:j]
+
+  G = preG.subgraph(nodes)
+  return G
+
+def OGB_to_nx(nodes, edges, features_n, name_features):
+  undirected = utils.is_undirected(edges)
 
   edge_list = []
-  for i in range(len(edges_ini[0])):
-    edge_list.append((int(edges_ini[0][i]), int(edges_ini[1][i])))
+  for i in range(len(edges[0])):
+    edge_list.append((int(edges[0][i]), int(edges[1][i])))
 
   if undirected:
     G = nx.Graph()
   else:
     G = nx.DiGraph()
 
-  G.add_nodes_from(nodes_ini)
+  G.add_nodes_from(nodes)
   G.add_edges_from(edge_list)
-  nx.set_node_attributes(G, features) #controlar que es una lista de ints
+
+  #print(features_n)
+  features = [int(f) for f in features_n]
+  dict_features = list_to_dict(features)
+  #print(dict_features)
+  nx.set_node_attributes(G, dict_features, name_features) #controlar que es una lista de ints
 
   return (G, undirected)
 
-def planetoid_to_nx(dataset):
+def planetoid_to_nx(dataset, features_t, name_features):
+  #print(features_t)
   D = Data(dataset[0].x, dataset[0].edge_index, dataset[0].y)
   #print(dataset[0].edge_index)
-  G = utils.to_networkx(D, ['x', 'y']) #to_undirected, default always directed
+  G = utils.to_networkx(D) #to_undirected, default always directed
 
+  features = [int(f) for f in features_t]
+  dict_features = list_to_dict(features)
+  nx.set_node_attributes(G, dict_features, name_features)
   undirected = not nx.is_directed(G)
 
   return G, undirected
@@ -127,8 +148,16 @@ def get_biggest_CC(G, undirected):
   cc = CC[0]
 
   values_dict['Num CC'].append(num_CC)
+  values_dict['BCC num nodes'].append(cc.number_of_nodes())
+  values_dict['BCC num edges'].append(cc.number_of_edges())
 
   return cc
+
+def list_to_dict(l):
+  nums = list(range(0,len(l)))
+  dictionary = dict(zip(nums,l))
+
+  return dictionary
 
 def ini_dict(fsplit, name):
   values_dict['Dataset name'] = name
@@ -193,40 +222,22 @@ def write_csv_mean():
       w.writeheader()
       w.writerow(values_dict)
 
-def graph_to_characterization(G, G_all, nodes, undirected):
-  values_dict['Directed'] = (not undirected)
+def compute_assortativity(G, undirected, name_features):
 
   time_ini = time.time()
 
-  print('Graph processing...')
-  graph_processing(G, G_all, nodes, undirected)
+  assortativity = nx.attribute_assortativity_coefficient(G, name_features)
   cc = get_biggest_CC(G, undirected)
-  print('CC processing...')
-  CC_processing(cc, undirected)
-  print('Communities processing...')
-  community_detection(cc, undirected)
-
+  cc_assortativity = nx.attribute_assortativity_coefficient(cc, name_features)
+  
   time_end = time.time() - time_ini
 
+  values_dict['Homophily'].append(assortativity)
+  values_dict['BCC homophily'].append(cc_assortativity)
   values_dict['Execution time'].append(time_end)
 
-def split_to_results(name, G_all, nodes_ini, undirected, i):
-  G, nodes = second_split(G_all, nodes_ini, i)
-  print('Graph generated')
-
-  if METRICS: 
-    graph_to_characterization(G, G_all, nodes, undirected)
-
-  if DEGREE_HIST:
-    print('Generating degree plots...')
-    histogram_degree(G, G_all, nodes, undirected, i)
-
-  if (not METRICS) and COM_HIST:
-    cc = get_biggest_CC(G, undirected)
-    community_detection(cc, undirected)
-
-def split_control(name, data, dataset, fsplit):
-  nodes_ini, edges_ini, features = first_split(data, dataset, fsplit)
+def split_control(data, dataset, fsplit):
+  nodes_ini, edges_ini, features, name_features = first_split(data, dataset, fsplit)
   choose_globals(len(nodes_ini))
 
   if TYPE_SPLIT == 'random': 
@@ -242,27 +253,36 @@ def split_control(name, data, dataset, fsplit):
     print(step)
     values_dict['Type split'] = str(num_it) + ' ordered iterations'
 
-  if data == 'OGB':
-    G, undirected = OGB_to_nx(nodes_ini, edges_ini, features)
-  elif data == 'Plan':
-    G, undirected = planetoid_to_nx(dataset, features)
+  if data == 'Plan':
+    preG, undirected = planetoid_to_nx(dataset, features, name_features)
     
   for i in range(0, lim, step):
     print('Iteration ' + str(i))
-    split_to_results(name, G_all, nodes_ini, undirected, i)
+
+    if data == 'OGB':
+      nodes, edges = second_split_OGB(nodes_ini, edges_ini, i)
+      G, undirected = OGB_to_nx(nodes, edges, features, name_features)
+    elif data == 'Plan':
+      G = second_split_planetoid(preG, nodes_ini, i)
+    
+    #print(nx.get_node_attributes(G, 'y'))
+    values_dict['Directed'] = (not undirected)
+    values_dict['Num nodes'].append(G.number_of_nodes())
+    values_dict['Num edges'].append(G.number_of_edges())
+    
+    compute_assortativity(G, undirected, name_features)
   
-  if METRICS:
-    print('Writing metrics...')
-    if METRICS_RESULTS == 'mean':
-      mean_dict()
-      write_csv_mean()
-      
-    elif METRICS_RESULTS == 'all':
-      write_csv_all()
+  print('Writing metrics...')
+  if METRICS_RESULTS == 'mean':
+    mean_dict()
+    write_csv_mean()
+    
+  elif METRICS_RESULTS == 'all':
+    write_csv_all()
 
 def main():
-  name, data, dataset, fsplit = choose_dataset()
-  split_control(name, data, dataset, fsplit)
+  data, dataset, fsplit = choose_dataset()
+  split_control(data, dataset, fsplit)
 
 
 
@@ -283,7 +303,7 @@ def main():
 #   return (nodes, edges)
 
 def test():
-  dataset = Planetoid(name='Cora', root='/home/ctubert/tfg/gitprojects/gnn_analysis/analysis/datasets')
+  dataset = Planetoid(name='CiteSeer', root='/home/ctubert/tfg/gitprojects/gnn_analysis/analysis/datasets')
   print(utils.homophily_ratio(dataset[0].edge_index, dataset[0].y))
   D = Data(dataset[0].x, dataset[0].edge_index, dataset[0].y)
   print(dataset[0])
